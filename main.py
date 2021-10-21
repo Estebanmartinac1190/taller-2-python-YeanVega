0db08f2bb147ddc848f286ab90357e451dfa7023
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-10-21 17:12:44 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ class Asiento:
 
 class Auto:
     
-    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+    def __init__(self, modelo, precio, Asiento, marca, motor, registro, cantidadCreados):
         self.modelo = modelo
         self.precio = precio
-        self.asientos = asientos[0]
+        self.asientos = Asiento.asientos[0]
         self.marca = marca
         self.motor = motor
         self.registro = registro
@@ -24,16 +24,16 @@ class Auto:
 
 
     def verificarIntegridad(self, registro):
-        if Asiento.asiento[0] == registro and Motor.registro:
+        if Asiento.registro == registro and Motor.registro == registro:
             return "Auto original"
         else:
             return "Las piezas no son originales"
 
-    def cantidadAsientos():
+    def cantidadAsientos(self, Asiento):
         numeroAsientos = 0
-        if Asiento.asientos != []:
+        if Asiento.asientos != None:
             for asiento in Asiento.asientos:
-                if asiento != []:
+                if asiento != None:
                     numeroAsientos += 1
         return numeroAsientos
 
