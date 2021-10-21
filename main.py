6438868978236c994424c811b6ae24fa6dcb98ad
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-10-21 16:40:09 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 class Asiento:
-    color = " "
-    precio = 0
-    registro = 0
+
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
 
     def cambiarColor(self, color):
         if color == "rojo" or color == "verde" or color == "amarillo" or color == "blanco" or color == "negro":
@@ -10,13 +12,16 @@ class Asiento:
 
 
 class Auto:
-    modelo = " "
-    precio = 0
-    Asiento.asientos = []
-    marca = " "
-    motor = " "
-    registro = " "
-    cantidadCreados = 0
+    
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos[0]
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
+
 
     def verificarIntegridad(self, registro):
         if Asiento.asiento[0] == registro and Motor.registro:
@@ -33,9 +38,11 @@ class Auto:
         return numeroAsientos
 
 class Motor:
-    tipo = ""
-    numeroCilindros = 0
-    registro = 0
+    
+    def __init__(self, tipo, numeroCilindros, registro):
+        self.tipo = tipo
+        self.numeroCilindros = numeroCilindros
+        self.registro = registro
 
     def cambiarRegistro(self, registro):
         self.registro = registro
